189329f427a112c1c7fd9166db3d9856174f6ca0
Sunwha <ohsw17@naver.com>
2023-07-29 14:43:30 +0900

    axios

--- src/App.js
+++ src/App.js
@@ -11,39 +11,55 @@ import axios from "axios";
 import "./App.css";
 
 function App() {
-    const [shoes, setShoes] = useState(data);
+  const [shoes, setShoes] = useState(data);
+  const [more, setMore] = useState(0);
+  const [loading, setLoading] = useState(false);
 
-    function moreData() {
-        axios
-            .get("https://codingapple1.github.io/shop/data2.json")
-            .then((data) => {
-              // console.log(data.data);
-              let copy = [...shoes];
-                setShoes(copy.concat(data.data))
-            })
-            .catch(() => {
-                console.log("false");
-            });
-    }
-    return (
-        <div className="App">
-            <GnB />
-            <Routes>
-                <Route path="/" element={<Main shoes={shoes} moreData={moreData} />} />
-                <Route path="/detail/:id" element={<Detail shoes={shoes} />} />
-                <Route path="/about" element={<About />}>
-                    <Route path="member" element={<div>Member</div>} />
-                    <Route path="location" element={<div>Location</div>} />
-                </Route>
-                <Route path="/event" element={<Event />}>
-                    <Route index element={<div>첫 주문시 양배추즙 서비스</div>} />
-                    <Route path="one" element={<div>첫 주문시 양배추즙 서비스</div>} />
-                    <Route path="two" element={<div>생일기념 쿠폰 받기</div>} />
-                </Route>
-                <Route path="*" element={<Container>404</Container>} />
-            </Routes>
-        </div>
-    );
+  function moreData() {
+    setLoading(true);
+    setTimeout(() => {
+      axios
+        .get(`https://codingapple1.github.io/shop/data${more + 2}.json`)
+        .then((result) => {
+          let copy = [...shoes, ...result.data];
+          setShoes(copy);
+          setMore(more + 1);
+          setLoading(false);
+        })
+        .catch(() => {
+          console.log("false");
+        });
+    }, 1000);
+  }
+  return (
+    <div className="App">
+      <GnB />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Main
+              shoes={shoes}
+              moreData={moreData}
+              more={more}
+              loading={loading}
+            />
+          }
+        />
+        <Route path="/detail/:id" element={<Detail shoes={shoes} />} />
+        <Route path="/about" element={<About />}>
+          <Route path="member" element={<div>Member</div>} />
+          <Route path="location" element={<div>Location</div>} />
+        </Route>
+        <Route path="/event" element={<Event />}>
+          <Route index element={<div>첫 주문시 양배추즙 서비스</div>} />
+          <Route path="one" element={<div>첫 주문시 양배추즙 서비스</div>} />
+          <Route path="two" element={<div>생일기념 쿠폰 받기</div>} />
+        </Route>
+        <Route path="*" element={<Container>404</Container>} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
--- src/routes/Main.js
+++ src/routes/Main.js
@@ -1,35 +1,69 @@
 import { Container, Row, Col, Button } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 
-const Main = ({ shoes, moreData }) => {
-    return (
-        <>
-            <div className="main-bg" style={{ background: `url(${process.env.PUBLIC_URL}'/images/bg.png') center center no-repeat`, backgroundSize: "cover" }}></div>
+const Main = ({ shoes, moreData, more, loading }) => {
+  return (
+    <>
+      <div
+        className="main-bg"
+        style={{
+          background: `url(${process.env.PUBLIC_URL}'/images/bg.png') center center no-repeat`,
+          backgroundSize: "cover",
+        }}
+      ></div>
 
-            <Container>
-                <Row>
-                    {shoes.map((item, idx) => (
-                        <Shoes key={item.id} id={item.id} title={item.title} price={item.price} content={item.content} idx={idx} />
-                    ))}
-                </Row>
-                <p style={{ textAlign: "center", marginTop: "30px" }}>
-                    <Button variant="light" onClick={moreData}>More</Button>
-                </p>
-            </Container>
-        </>
-    );
+      <Container>
+        <Row md={3}>
+          {shoes.map((item, idx) => (
+            <Shoes
+              key={item.id}
+              id={item.id}
+              title={item.title}
+              price={item.price}
+              content={item.content}
+              idx={idx}
+            />
+          ))}
+        </Row>
+        <p
+          style={{
+            textAlign: "center",
+            marginTop: "30px",
+            marginBottom: "50px",
+          }}
+        >
+          {more == 2 ? (
+            <span>No more products</span>
+          ) : loading ? (
+            <span>loading...</span>
+          ) : (
+            <Button variant="light" onClick={moreData}>
+              More
+            </Button>
+          )}
+        </p>
+      </Container>
+    </>
+  );
 };
 
 function Shoes({ title, price, content, idx }) {
-    let navigate = useNavigate();
-    return (
-        <Col onClick={() => navigate(`/detail/${idx}`)}>
-            <img src={`${process.env.PUBLIC_URL}/images/shoes${idx + 1}.jpg`} alt={`shoes${idx + 1}`} width="80%" />
-            <h4>{title}</h4>
-            <p>{price} won</p>
-            <p>{content}</p>
-        </Col>
-    );
+  let navigate = useNavigate();
+  return (
+    <Col
+      onClick={() => navigate(`/detail/${idx}`)}
+      style={{ textAlign: "center" }}
+    >
+      <img
+        src={`${process.env.PUBLIC_URL}/images/shoes${idx + 1}.jpg`}
+        alt={`shoes${idx + 1}`}
+        width="80%"
+      />
+      <h4>{title}</h4>
+      <p>{price} won</p>
+      <p>{content}</p>
+    </Col>
+  );
 }
 
 export default Main;
